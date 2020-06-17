d4b8000c3a1adbdf640a6d134aae85a83c56f932
Daniel Formigoni <d.soares@aluno.ifsp.edu.br>
2020-06-17 09:38:43 -0300

    Updates

---
+++ precache-manifest.573990e6049cc013f759a26c3c39b48f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f57577d14996b03d743d16a5c317fcf3",
+    "url": "/movie_page/index.html"
+  },
+  {
+    "revision": "7550baf34f49dfbe7c25",
+    "url": "/movie_page/static/css/main.43bab27a.chunk.css"
+  },
+  {
+    "revision": "bd4417b453503b951f3c",
+    "url": "/movie_page/static/js/2.c5f4f51f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_page/static/js/2.c5f4f51f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7550baf34f49dfbe7c25",
+    "url": "/movie_page/static/js/main.a6b2cbb5.chunk.js"
+  },
+  {
+    "revision": "4e60ef11dbc429157cb9",
+    "url": "/movie_page/static/js/runtime-main.ee5971f3.js"
+  },
+  {
+    "revision": "08392f186e99a74794065effff6f90e5",
+    "url": "/movie_page/static/media/logo-tmdb.08392f18.svg"
+  }
+]);
